157a9815e7893958e5dc17e7131f183616cd05cd
PhamLeThanhNhiet <nhiethiz@gmail.com>
2025-02-08 18:34:03 +0700

    done UI LoginScreen v1.1

--- hnnt-chat-mobile/src/features/auth/screens/LoginScreen.js
+++ hnnt-chat-mobile/src/features/auth/screens/LoginScreen.js
@@ -1,20 +1,7 @@
-// import { View, Text, Button } from "react-native";
-// import { useNavigation } from "@react-navigation/native";
-
-// export default function LoginScreen() {
-//   const navigation = useNavigation();
-
-//   return (
-//     <View>
-//       <Text>Login Screen</Text>
-//       <Button title="Go to Home" onPress={() => navigation.navigate("HomeTab")} />
-//     </View>
-//   );
-// }
-
 import React, { useState } from 'react';
 import { View, Text, TextInput, StyleSheet, TouchableOpacity, KeyboardAvoidingView, Image } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function LoginScreen() {
   const navigation = useNavigation();
@@ -24,61 +11,68 @@ export default function LoginScreen() {
   const isFormFilled = phone.length > 0 && password.length > 0;
 
   return (
-    <KeyboardAvoidingView style={styles.container}>
-      <View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: '#D6EAF8' }}>
-        <TouchableOpacity style={{ paddingRight: 10 }} onPress={() => navigation.goBack()}>
-          <Image source={require('../../../assets/icons/back.png')} style={{ height: 30, width: 30 }} />
-        </TouchableOpacity>
-        <Text style={styles.title}>Login</Text>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <SafeAreaProvider>
+        <KeyboardAvoidingView>
+
+          <View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: '#D6EAF8', paddingTop: 10 }}>
+            <TouchableOpacity style={{ paddingRight: 10 }} onPress={() => navigation.goBack()}>
+              <Image source={require('../../../assets/icons/back.png')} style={{ height: 30, width: 30 }} />
+            </TouchableOpacity>
+            <Text style={styles.title}>Login</Text>
+          </View>
 
 
-      <View style={{ paddingHorizontal: 20, paddingTop: 20 }}>
-        <Text style={styles.description}>
-          Please enter your phone number and password to login
-        </Text>
-      </View>
+          <View style={{ paddingHorizontal: 20, paddingTop: 20 }}>
+            <Text style={styles.description}>
+              Please enter your phone number and password to login
+            </Text>
+          </View>
 
-      <View style={{ paddingHorizontal: 20 }}>
-        <TextInput
-          style={styles.input}
-          placeholder="Phone number"
-          value={phone}
-          onChangeText={setPhone}
-          keyboardType="phone-pad"
-        />
-      </View>
+          <View style={{ paddingHorizontal: 20 }}>
+            <TextInput
+              style={styles.input}
+              placeholder="Phone number"
+              value={phone}
+              onChangeText={setPhone}
+              keyboardType="phone-pad"
+            />
+          </View>
 
-      <View style={{ paddingHorizontal: 20 }}>
-        <TextInput
-          style={styles.input}
-          placeholder="password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry={true}
-        />
-      </View>
+          <View style={{ paddingHorizontal: 20 }}>
+            <TextInput
+              style={styles.input}
+              placeholder="password"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry={true}
+            />
+          </View>
 
-      <View style={{ paddingHorizontal: 20 }}>
-        <TouchableOpacity>
-          <Text style={styles.forgotPasswordText}>Forgot password?</Text>
-        </TouchableOpacity>
-      </View>
+          <View style={{ paddingHorizontal: 20 }}>
+            <TouchableOpacity>
+              <Text style={styles.forgotPasswordText}>Forgot password?</Text>
+            </TouchableOpacity>
+          </View>
 
-      {/* Nút "Next" */}
-      <TouchableOpacity
-        style={[styles.nextButton, isFormFilled ? styles.buttonEnabled : styles.buttonDisabled]}
-        disabled={!isFormFilled} // Chỉ bấm khi đã điền đầy đủ thông tin
-        onPress={() => {
-          if (isFormFilled) {
-            // Điều hướng hoặc xử lý đăng nhập
-          }
-        }}
-      >
-        <Text style={styles.nextButtonText}>→</Text>
-      </TouchableOpacity>
+          {/* Nút "Next" */}
+          <View style={{ paddingTop: 60 }}>
+            <TouchableOpacity
+              style={[styles.nextButton, isFormFilled ? styles.buttonEnabled : styles.buttonDisabled]}
+              disabled={!isFormFilled} // Chỉ bấm khi đã điền đầy đủ thông tin
+              onPress={() => {
+                if (isFormFilled) {
+                  // Điều hướng hoặc xử lý đăng nhập
+                }
+              }}
+            >
+              <Text style={styles.nextButtonText}>→</Text>
+            </TouchableOpacity>
+          </View>
 
-    </KeyboardAvoidingView>
+        </KeyboardAvoidingView>
+      </SafeAreaProvider>
+    </SafeAreaView>
   );
 }
 
