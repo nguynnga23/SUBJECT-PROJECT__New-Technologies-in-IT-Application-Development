b3c5ebe07f920d2b005f83ec9baeb51a485a296b
PhamLeThanhNhiet <nhiethiz@gmail.com>
2025-02-08 17:44:45 +0700

    done HomeScreen

---
+++ hnnt-chat-mobile/src/features/auth/screens/HomeScreen.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
+
+export default function HomeScreen() {
+    const navigation = useNavigation();
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Welcome to HNNT</Text>
+            
+            <View style={{ paddingTop: 40, paddingBottom: 40 }}>
+                <Image source={require('../../../assets/icons/img_HomeScreen.png')} style={styles.image} />
+            </View>
+            
+            <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('Login')}>
+                <Text style={styles.loginText}>Login</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.signUpButton} onPress={() => navigation.navigate('SignUp')}>
+                <Text style={styles.signUpText}>Create new account</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingHorizontal: 20,
+    },
+    title: {
+        paddingTop: 100,
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#333',
+        textAlign: 'center',
+        marginBottom: 5,
+    },
+    image: {
+        width: 200,
+        height: 200,
+        marginBottom: 20,
+        alignSelf: 'center',
+    },
+    loginButton: {
+        backgroundColor: '#007AFF',
+        paddingVertical: 12,
+        width: '100%',
+        borderRadius: 10,
+        alignItems: 'center',
+        marginBottom: 10,
+    },
+    loginText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    signUpButton: {
+        backgroundColor: '#E5E5E5',
+        paddingVertical: 12,
+        width: '100%',
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    signUpText: {
+        color: '#333',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
