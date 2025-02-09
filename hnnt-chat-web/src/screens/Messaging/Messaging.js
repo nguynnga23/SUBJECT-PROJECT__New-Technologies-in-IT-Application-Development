62e5234dc26e9f177e975a6470706b852f9d2008
youthofmay123 <nhathuy39337343@gmail.com>
2025-02-09 08:51:21 +0700

    add: file TabRightBar.js

--- hnnt-chat-web/src/screens/Messaging/Messaging.js
+++ hnnt-chat-web/src/screens/Messaging/Messaging.js
@@ -3,13 +3,13 @@ import { FaSearch } from 'react-icons/fa';
 import { RiUserAddLine } from 'react-icons/ri';
 import { AiOutlineUsergroupAdd } from 'react-icons/ai';
 import { TiDelete } from 'react-icons/ti';
-import { FaTrashAlt } from 'react-icons/fa';
 
 import PopupAddFriend from '../../components/Popup/PopupAddFriend';
 import PopupAddGroup from '../../components/Popup/PopupAddGroup';
 import TabSearch from '../../components/Tab/TabSearch';
 import TabMesssage from '../../components/Tab/TabMessage';
 import TabChat from '../../components/Tab/TabChat';
+import TabRightBar from '../../components/Tab/TabRightBar';
 
 function Messaging() {
     const [selectedChat, setSelectedChat] = useState(null);
@@ -20,8 +20,6 @@ function Messaging() {
     const [search, setSearch] = useState('');
 
     const [showRightBar, setShowRightBar] = useState(false);
-
-    const [activeMessageTab, setActiveMessageTab] = useState('info');
 
     return (
         <div className="h-screen flex flex-col">
@@ -81,87 +79,7 @@ function Messaging() {
                         </div>
                     )}
                 </div>
-                {showRightBar && (
-                    <div className="w-1/4 flex flex-col bg-white min-w-[330px] border-l">
-                        {/* Tabs */}
-                        <div className="flex border-b">
-                            <button
-                                className={`flex-1 py-3.5 text-center font-medium ${
-                                    activeMessageTab === 'info'
-                                        ? 'text-blue-600 border-b-2 border-blue-600'
-                                        : 'text-gray-500'
-                                }`}
-                                onClick={() => setActiveMessageTab('info')}
-                            >
-                                Thông tin
-                            </button>
-                            <button
-                                className={`flex-1 py-2 text-center font-medium ${
-                                    activeMessageTab === 'emoji'
-                                        ? 'text-blue-600 border-b-2 border-blue-600'
-                                        : 'text-gray-500'
-                                }`}
-                                onClick={() => setActiveMessageTab('emoji')}
-                            >
-                                Biểu tượng
-                            </button>
-                        </div>
-
-                        {/* Nội dung */}
-                        {activeMessageTab === 'info' && (
-                            <div className="p-4">
-                                {/* Avatar + Tên nhóm */}
-                                <div className="flex flex-col items-center mb-4">
-                                    <img
-                                        src="" // Thay bằng avatar thật
-                                        className="w-20 h-20 rounded-full border"
-                                    />
-                                    <h3 className="font-bold text-lg mt-2 font-bold">{selectedChat.name}</h3>
-                                </div>
-
-                                {/* Danh mục */}
-                                <div className="space-y-2">
-                                    <div className="p-2 border-b">
-                                        <div className="flex justify-between items-center ">
-                                            <span className=" font-bold cursor-pointer">Ảnh/Video</span>
-                                            <span className=" font-bold">{'>'}</span>
-                                        </div>
-                                        <p className="text-xs text-gray-400 mt-1 font-bold">
-                                            Chưa có Ảnh/Video được chia sẻ trong hội thoại này
-                                        </p>
-                                    </div>
-                                    <div className="p-2 border-b">
-                                        <div className="flex justify-between items-center ">
-                                            <span className="font-bold cursor-pointer">File</span>
-                                            <span className=" font-bold">{'>'}</span>
-                                        </div>
-                                        <p className="text-xs text-gray-400 mt-1 font-bold">
-                                            Chưa có File được chia sẻ trong hội thoại này
-                                        </p>
-                                    </div>
-                                    <div className="items-center p-2 border-b cursor-pointer">
-                                        <div className="flex justify-between items-center ">
-                                            <span className="text-gray-600 font-bold">Link</span>
-                                            <span className="font-bold">{'>'}</span>
-                                        </div>
-
-                                        <p className="text-xs text-gray-400 mt-1 font-bold">
-                                            Chưa có Link được chia sẻ trong hội thoại này
-                                        </p>
-                                    </div>
-                                </div>
-
-                                {/* Xóa lịch sử trò chuyện */}
-                                <div className="mt-4 text-red-500 flex items-center space-x-2 cursor-pointer">
-                                    <FaTrashAlt />
-                                    <span>Xóa lịch sử trò chuyện</span>
-                                </div>
-                            </div>
-                        )}
-
-                        {activeTab === 'emoji' && <div className="p-4 text-gray-500 text-center">Tab Biểu tượng</div>}
-                    </div>
-                )}
+                {showRightBar && <TabRightBar activeTab={activeTab} selectedChat={selectedChat} />}
             </div>
         </div>
     );
