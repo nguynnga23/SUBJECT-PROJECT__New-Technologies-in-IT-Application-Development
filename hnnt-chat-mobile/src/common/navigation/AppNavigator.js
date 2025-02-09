57e38b626a1562c436caf4e4c7174124a5c91379
Nguyen Nga <nguyennga200x@gmail.com>
2025-02-09 13:11:41 +0700

    feat: Update AppNavigator to include new authentication screens and set HomeScreen as initial route

--- hnnt-chat-mobile/src/common/navigation/AppNavigator.js
+++ hnnt-chat-mobile/src/common/navigation/AppNavigator.js
@@ -7,14 +7,24 @@ import PhoneBookScreen from '../../features/contact/screens/friends/phonebook/Ph
 import AddFriendScreen from '../screens/AddFriendScreen';
 import AddGroupScreen from '../screens/AddGroupScreen';
 import SearchScreen from '../screens/SearchScreen';
+import PhoneNumSignUpScreen from '../../features/auth/screens/PhoneNumSignUpScreen';
+import OTPConfirmScreen from '../../features/auth/screens/OTPConfirmScreen';
+import PasswordSignUpScreen from '../../features/auth/screens/PasswordSignUpScreen';
+import HomeScreen from '../../features/auth/screens/HomeScreen';
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
+            <Stack.Navigator initialRouteName="HomeScreen">
+                <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
                 <Stack.Screen name="Login" component={LoginScreen} />
                 <Stack.Screen name="HomeTab" component={BottomTabNavigator} options={{ headerShown: false }} />
+
+                <Stack.Screen name="PhoneNumSignUp" component={PhoneNumSignUpScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="OTPConfirm" component={OTPConfirmScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="PasswordSignUp" component={PasswordSignUpScreen} options={{ headerShown: false }} />
+
                 <Stack.Screen name="FriendRequest" component={FriendRequestScreen} />
                 <Stack.Screen name="PhoneBook" component={PhoneBookScreen} />
                 <Stack.Screen name="AddFriend" component={AddFriendScreen} />
