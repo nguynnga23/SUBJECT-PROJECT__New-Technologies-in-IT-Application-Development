47a2192b2304b1a5754ce5019e7616b1779beda2
Nguyen Nga <nguyennga200x@gmail.com>
2025-02-12 21:07:04 +0700

    feat: Enhance AddFriendScreen with phone number input and action items

--- hnnt-chat-mobile/src/common/screens/AddFriendScreen.js
+++ hnnt-chat-mobile/src/common/screens/AddFriendScreen.js
@@ -1,6 +1,18 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
+import PhoneInput from 'react-native-phone-number-input';
+import AntDesign from '@expo/vector-icons/AntDesign';
+const ActionItem = ({ title, onPress, iconName }) => (
+    <TouchableOpacity style={styles.actionItem} onPress={onPress} activeOpacity={0.7}>
+        <View style={styles.customIcon}>
+            <FontAwesome name={iconName} size={24} color="#007AFF" />
+        </View>
+        <Text style={styles.actionText}>{title}</Text>
+    </TouchableOpacity>
+);
 export default function AddFriendScreen() {
     return (
         <View style={styles.container}>
@@ -9,7 +21,46 @@ export default function AddFriendScreen() {
                 <View></View>
                 <Text>Quét mã để thêm bạn Zalo với tôi</Text>
             </View>
-            <View style={styles.enterNumberPhoneWrapper}></View>
+            <View style={styles.enterNumberPhoneWrapper}>
+                <View style={styles.phoneInputWrapper}>
+                    <View>
+                        <PhoneInput
+                            defaultCode="VN"
+                            layout="second"
+                            containerStyle={{
+                                backgroundColor: '#fff',
+                                height: 50,
+                                borderRadius: 10,
+                                borderWidth: 1,
+                                borderColor: '#ddd',
+                                padding: 10,
+                                alignSelf: 'center',
+                            }}
+                            textContainerStyle={{
+                                backgroundColor: '#f9f9f9',
+                                borderRadius: 8,
+                            }}
+                            textInputStyle={{
+                                color: '#333',
+                                fontSize: 16,
+                            }}
+                            codeTextStyle={{
+                                color: '#000',
+                                fontWeight: 'bold',
+                            }}
+                            withDarkTheme
+                            withShadow
+                            autoFocus
+                        />
+                    </View>
+                    <TouchableOpacity style={styles.arrowRightButton}>
+                        <AntDesign name="arrowright" size={24} color="black" />
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <ActionItem title="Scan QR code" iconName="qrcode" />
+                </View>
+            </View>
             <View style={styles.actionWrapper}></View>
             <View style={styles.viewSendRequestWrapper}></View>
         </View>
@@ -26,10 +77,52 @@ const styles = StyleSheet.create({
     },
     enterNumberPhoneWrapper: {
         flex: 1,
+        justifyContent: 'space-around',
+        backgroundColor: '#fff',
+        // alignItems: 'center',
+    },
+    phoneInputWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        alignItems: 'center',
+        borderBottomWidth: 1,
+    },
+    arrowRightButton: {
+        backgroundColor: '#DEE3E7',
+        height: 40,
+        width: 40,
+        borderRadius: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     actionWrapper: {
         flex: 1,
         backgroundColor: 'pink',
+    },
+    // Action Section
+    actionWrapper: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'space-around',
+        marginBottom: 10,
+        paddingHorizontal: 15,
+    },
+    actionItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    customIcon: {
+        width: 35,
+        height: 35,
+        borderRadius: 12,
+        marginRight: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    actionText: {
+        textAlign: 'center',
     },
     viewSendRequestWrapper: {
         flex: 1,
