c0a87830ebb8cd953f29aa9f1f99eff3dfeab682
skyfourhmmm <nguyenthientu413.com>
2025-02-13 18:54:39 +0700

    fix profile

--- hnnt-chat-web/src/components/Modal.js
+++ hnnt-chat-web/src/components/Modal.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import CoverPhoto from '../public/cover_photo_sample.jpg';
 import Avatar from '../public/avatar_sample.jpg';
 
@@ -9,21 +9,78 @@ import { MdOutlineInsertPhoto } from 'react-icons/md';
 
 import { motion } from 'framer-motion';
 
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
 function Modal({ isOpen, onClose }) {
     const [isType, setIsType] = useState('profile');
     const [isName, setIsName] = useState('Nguyễn Thiên Tứ');
     const [gender, setGender] = useState('male');
 
+    // -----------------------------------------------------
+    const currentYear = new Date().getFullYear();
+
+    const [day, setDay] = useState(31);
+    const [month, setMonth] = useState(1);
+    const [year, setYear] = useState(2025);
+    const [maxDays, setMaxDays] = useState(31);
+
+    // Hàm kiểm tra số ngày hợp lệ trong tháng
+    const getMaxDays = (month, year) => {
+        if ([4, 6, 9, 11].includes(month)) return 30; // Tháng 4, 6, 9, 11 có 30 ngày
+        if (month === 2) return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0) ? 29 : 28; // Tháng 2
+        return 31; // Các tháng còn lại có 31 ngày
+    };
+
+    // Cập nhật số ngày hợp lệ khi tháng hoặc năm thay đổi
+    useEffect(() => {
+        const newMaxDays = getMaxDays(month, year);
+        setMaxDays(newMaxDays);
+
+        if (day > newMaxDays) {
+            setDay(newMaxDays); // Giữ ngày trong phạm vi hợp lệ
+        }
+    }, [month, year]);
+
+    // lấy ảnh từ máy
+    const fileInputRef = useRef(null);
+
+    const handleTakePhoto = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    };
+
+    const [image, setImage] = useState(null);
+
+    // Đống mở modal
     if (!isOpen) return null;
 
     return (
         <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
-            <div className="bg-white rounded-md shadow-lg py-2 w-full max-w-sm h-[80vh] flex flex-col relative overflow-hidden">
+            <motion.div
+                initial={{ scale: 0.5, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                exit={{ scale: 0.5, opacity: 0 }}
+                transition={{ duration: 0.3, ease: 'easeOut' }}
+                className="bg-white rounded-md shadow-lg py-2 w-full max-w-sm h-[80vh] flex flex-col relative overflow-hidden"
+            >
                 <div className="flex justify-between px-4 items-center mb-4">
-                    {isType === 'avatar' ? (
+                    {isType === 'avatar' || isType === 'editAvatar' ? (
                         <div className="flex items-center gap-2">
                             <button
-                                onClick={() => setIsType('profile')}
+                                onClick={() => {
+                                    if (isType === 'avatar') {
+                                        setIsType('profile');
+                                    } else {
+                                        setIsType('avatar');
+                                    }
+                                }}
                                 className="w-8 h-8 flex items-center justify-center text-black hover:text-gray-700 hover:bg-gray-200 rounded-full text-xl"
                             >
                                 <IoIosArrowBack />
@@ -44,7 +101,10 @@ function Modal({ isOpen, onClose }) {
                         <p className="text-md font-semibold">Thông tin tài khoản</p>
                     )}
                     <button
-                        onClick={onClose}
+                        onClick={() => {
+                            onClose();
+                            setIsType('profile');
+                        }}
                         className="w-8 h-8 flex items-center justify-center text-black hover:text-gray-700 hover:bg-gray-200 rounded-full text-xl"
                     >
                         ✕
@@ -59,8 +119,22 @@ function Modal({ isOpen, onClose }) {
                         transition={{ duration: 0.4, ease: 'easeInOut' }}
                     >
                         <div className="border-t pt-2 flex justify-center">
+                            <input
+                                type="file"
+                                accept="image/*"
+                                ref={fileInputRef}
+                                className="hidden"
+                                onChange={(e) => {
+                                    const file = e.target.files[0];
+                                    if (file) {
+                                        const imageUrl = URL.createObjectURL(file); // Tạo URL tạm thời
+                                        setImage(imageUrl);
+                                        setIsType('editAvatar');
+                                    }
+                                }}
+                            />
                             <button
-                                onClick={onClose}
+                                onClick={handleTakePhoto}
                                 className="w-11/12 text-blue-800 bg-blue-200 hover:bg-blue-300 rounded-md flex items-center justify-center gap-2 p-2 cursor-pointer"
                             >
                                 <MdOutlineInsertPhoto className="cursor-pointer" />
@@ -119,74 +193,81 @@ function Modal({ isOpen, onClose }) {
 
                             <p className="text-base font-semibold mt-6">Thông tin cá nhân</p>
                             <div className="flex space-x-4 mt-4 ">
-                                {/* Radio Nam */}
-                                <label className="flex items-center space-x-2 cursor-pointer">
-                                    <input
-                                        type="radio"
-                                        name="gender"
-                                        value="male"
-                                        checked={gender === 'male'}
-                                        onChange={() => setGender('male')}
-                                        className="w-4 h-4 text-blue-600 border-blue-500 focus:ring-blue-500"
-                                    />
-                                    <span className="text-gray-700">Nam</span>
-                                </label>
-
-                                {/* Radio Nữ */}
-                                <label className="flex items-center space-x-2 cursor-pointer">
-                                    <input
-                                        type="radio"
-                                        name="gender"
-                                        value="female"
-                                        checked={gender === 'female'}
-                                        onChange={() => setGender('female')}
-                                        className="w-4 h-4 text-blue-600 border-blue-500 focus:ring-blue-500"
-                                    />
-                                    <span className="text-gray-700">Nữ</span>
-                                </label>
+                                {/* Giới tính */}
+                                <FormControl>
+                                    <RadioGroup
+                                        row
+                                        aria-labelledby="demo-row-radio-buttons-group-label"
+                                        name="row-radio-buttons-group"
+                                        value={gender}
+                                        onChange={(e) => setGender(e.target.value)}
+                                    >
+                                        <FormControlLabel
+                                            className="text-gray-700"
+                                            value="male"
+                                            control={<Radio />}
+                                            label="Nam"
+                                        />
+                                        <FormControlLabel
+                                            className="text-gray-700"
+                                            value="female"
+                                            control={<Radio />}
+                                            label="Nữ"
+                                        />
+                                    </RadioGroup>
+                                </FormControl>
                             </div>
 
                             {/* ngày sinh */}
                             <p className="text-sm mt-4 mb-2">Ngày sinh</p>
-                            <div className="flex gap-2 w-full">
+                            <div className="flex gap-2 w-full h-10">
                                 {/* Ngày */}
-                                <select
+                                <Select
+                                    value={day}
+                                    onChange={(e) => setDay(e.target.value)}
                                     style={{ minWidth: '30%' }}
-                                    className="p-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500"
+                                    MenuProps={{ PaperProps: { sx: { maxHeight: 230 } } }}
+                                    className="p-2 border rounded-md focus:ring-2 focus:ring-blue-500"
                                 >
-                                    {Array.from({ length: 31 }, (_, i) => (
-                                        <option key={i + 1} value={i + 1}>
+                                    {Array.from({ length: maxDays }, (_, i) => (
+                                        <MenuItem key={i + 1} value={i + 1}>
                                             {i + 1}
-                                        </option>
+                                        </MenuItem>
                                     ))}
-                                </select>
+                                </Select>
 
                                 {/* Tháng */}
-                                <select
+                                <Select
+                                    value={month}
+                                    onChange={(e) => setMonth(e.target.value)}
                                     style={{ minWidth: '30%' }}
-                                    className="p-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500"
+                                    MenuProps={{ PaperProps: { sx: { maxHeight: 230 } } }}
+                                    className="p-2 border rounded-md focus:ring-2 focus:ring-blue-500"
                                 >
                                     {Array.from({ length: 12 }, (_, i) => (
-                                        <option key={i + 1} value={i + 1}>
+                                        <MenuItem key={i + 1} value={i + 1}>
                                             {i + 1}
-                                        </option>
+                                        </MenuItem>
                                     ))}
-                                </select>
+                                </Select>
 
                                 {/* Năm */}
-                                <select
+                                <Select
+                                    value={year}
+                                    onChange={(e) => setYear(e.target.value)}
                                     style={{ minWidth: '35%' }}
-                                    className="p-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500"
+                                    MenuProps={{ PaperProps: { sx: { maxHeight: 230 } } }}
+                                    className="p-2 border rounded-md focus:ring-2 focus:ring-blue-500"
                                 >
                                     {Array.from({ length: 100 }, (_, i) => {
-                                        const year = new Date().getFullYear() - i;
+                                        const y = currentYear - i;
                                         return (
-                                            <option key={year} value={year}>
-                                                {year}
-                                            </option>
+                                            <MenuItem key={y} value={y}>
+                                                {y}
+                                            </MenuItem>
                                         );
                                     })}
-                                </select>
+                                </Select>
                             </div>
                         </div>
 
@@ -207,6 +288,42 @@ function Modal({ isOpen, onClose }) {
                             </button>
                         </div>
                     </motion.div>
+                ) : isType === 'editAvatar' ? (
+                    image && (
+                        <>
+                            <div className="flex-1 overflow-auto pt-6 border-t border-gray-200 relative">
+                                <div className="w-full flex justify-center relative">
+                                    {/* Ảnh hiển thị */}
+                                    <img src={image} alt="Preview" className="mt-2 max-w-full h-80 object-cover " />
+
+                                    {/* Lớp mờ với vùng tròn trong suốt */}
+                                    <div
+                                        className="absolute inset-0 bg-black/50"
+                                        style={{
+                                            WebkitMaskImage: `radial-gradient(circle 160px at center, transparent 160px, black 161px)`,
+                                            maskImage: `radial-gradient(circle 160px at center, transparent 160px, black 161px)`,
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                            {/* button cập nhật */}
+                            <div className="border-t pt-2 flex justify-end mt-auto">
+                                <button
+                                    onClick={() => setIsType('avatar')}
+                                    className="bg-gray-200 hover:bg-gray-300 rounded-md flex items-center justify-center gap-2 p-2 cursor-pointer mr-2"
+                                >
+                                    <p className="font-semibold">Hủy</p>
+                                </button>
+
+                                <button
+                                    onClick={onClose}
+                                    className="bg-blue-600 hover:bg-blue-800 rounded-md flex items-center justify-center gap-2 p-2 cursor-pointer mr-3"
+                                >
+                                    <p className="font-semibold text-white">Cập nhật</p>
+                                </button>
+                            </div>
+                        </>
+                    )
                 ) : (
                     <>
                         <div className="flex-1 overflow-auto relative">
@@ -255,7 +372,7 @@ function Modal({ isOpen, onClose }) {
 
                         <div className="border-t pt-2 flex justify-center">
                             <button
-                                onClick={onClose}
+                                onClick={() => setIsType('edit')}
                                 className="w-11/12 hover:bg-gray-200 rounded-md flex items-center justify-center gap-2 p-2 cursor-pointer"
                             >
                                 <LuPencilLine className="cursor-pointer" />
@@ -264,7 +381,7 @@ function Modal({ isOpen, onClose }) {
                         </div>
                     </>
                 )}
-            </div>
+            </motion.div>
         </div>
     );
 }
