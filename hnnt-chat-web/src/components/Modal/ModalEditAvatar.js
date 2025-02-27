1eb8e8ab35c900c58067419a7c22f0bf2c28edb8
skyfourhmmm <nguyenthientu413.com>
2025-02-27 16:19:51 +0700

    chore:handle zoom image avatar

--- hnnt-chat-web/src/components/Modal/ModalEditAvatar.js
+++ hnnt-chat-web/src/components/Modal/ModalEditAvatar.js
@@ -1,10 +1,19 @@
+import { useState } from 'react';
+
 function ModalEditAvatar({ image, setIsType, onClose }) {
+    const [zoom, setZoom] = useState(1);
+
     return (
         <>
             <div className="flex-1 overflow-auto pt-6 border-t border-gray-200 relative">
-                <div className="w-full flex justify-center relative">
+                <div className="w-full flex justify-center relative overflow-hidden">
                     {/* Ảnh hiển thị */}
-                    <img src={image} alt="Preview" className="mt-2 max-w-full h-80 object-cover " />
+                    <img
+                        src={image}
+                        alt="Preview"
+                        className="mt-2 max-w-full h-80 object-cover cursor-pointer"
+                        style={{ transform: `scale(${zoom})` }}
+                    />
 
                     {/* Lớp mờ với vùng tròn trong suốt */}
                     <div
@@ -16,6 +25,26 @@ function ModalEditAvatar({ image, setIsType, onClose }) {
                     />
                 </div>
             </div>
+
+            {/* Thanh trượt */}
+            <div className="flex items-center my-10 w-80 mx-auto">
+                <button className="text-xl px-2" onClick={() => setZoom((prev) => Math.max(1, prev - 0.1))}>
+                    −
+                </button>
+                <input
+                    type="range"
+                    min="1"
+                    max="2"
+                    step="0.1"
+                    value={zoom}
+                    onChange={(e) => setZoom(parseFloat(e.target.value))}
+                    className="w-full accent-gray-600 cursor-pointer bg-blue-500"
+                />
+                <button className="text-xl px-2" onClick={() => setZoom((prev) => Math.min(2, prev + 0.1))}>
+                    +
+                </button>
+            </div>
+
             {/* button cập nhật */}
             <div className="border-t pt-2 flex justify-end mt-auto">
                 <button
