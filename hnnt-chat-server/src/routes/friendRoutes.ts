48fb53d29f017238b63ffd1d6620d19e2b7753de
Nguyen Nga <nguyennga200x@gmail.com>
2025-03-27 23:16:42 +0700

    feat: add authentication middleware to friend routes for enhanced security

--- hnnt-chat-server/src/routes/friendRoutes.ts
+++ hnnt-chat-server/src/routes/friendRoutes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { authenticate } from '../middleware/auth';
+
 import {
     sendFriendRequest,
     cancelFriendRequest,
@@ -13,14 +15,14 @@ import {
 
 const friendRouter = Router();
 
-friendRouter.post('/request', sendFriendRequest); //User1 request friend to User2
-friendRouter.delete('/request/cancel/:id', cancelFriendRequest); // User1/User2 cancel request
-friendRouter.post('/request/accept/:id', acceptFriendRequest); // User2 accept request User1
-friendRouter.delete('/delete/:id', deleteFriend); // User1/User2 delete friendship relationship
-friendRouter.get('/list/:userId', getListFriend); // Get list friends by userId
-friendRouter.get('/request/:userId', getListFriendRequest); // Get list friend request by userId
-friendRouter.post('/user/block', blockRequest); // Block user
-friendRouter.get('/user/block/list/:userId', ListBlockRequest); // Get list block requests
-friendRouter.delete('/user/block/:id', CancelBlockRequest); // Cancel block request
+friendRouter.post('/request', authenticate, sendFriendRequest); //User1 request friend to User2
+friendRouter.delete('/request/cancel/:id', authenticate, cancelFriendRequest); // User1/User2 cancel request
+friendRouter.post('/request/accept/:id', authenticate, acceptFriendRequest); // User2 accept request User1
+friendRouter.delete('/delete/:id', authenticate, deleteFriend); // User1/User2 delete friendship relationship
+friendRouter.get('/list', authenticate, getListFriend); // Get list friends by userId
+friendRouter.get('/request/:userId', authenticate, getListFriendRequest); // Get list friend request by userId
+friendRouter.post('/user/block', authenticate, blockRequest); // Block user
+friendRouter.get('/user/block/list/:userId', authenticate, ListBlockRequest); // Get list block requests
+friendRouter.delete('/user/block/:id', authenticate, CancelBlockRequest); // Cancel block request
 
 export default friendRouter;
