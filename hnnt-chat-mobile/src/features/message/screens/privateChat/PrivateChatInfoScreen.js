d0b03ec4c4c7ed501b532812581ed1d1845532ed
PhamLeThanhNhiet <nhiethiz@gmail.com>
2025-04-15 09:10:05 +0700

    feat: Enhance PrivateChatInfoScreen with chat fetching, pinning, and unpinning functionalities

--- hnnt-chat-mobile/src/features/message/screens/privateChat/PrivateChatInfoScreen.js
+++ hnnt-chat-mobile/src/features/message/screens/privateChat/PrivateChatInfoScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import {
     View,
     Text,
@@ -11,38 +11,129 @@ import {
     TextInput,
     Button,
     TouchableWithoutFeedback,
+    Alert
 } from 'react-native';
 import { Ionicons, AntDesign } from '@expo/vector-icons';
-import { useRoute } from '@react-navigation/native';
+import { useRoute, useFocusEffect } from '@react-navigation/native';
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { handleBlock, handleReport, toggleMute } from '../../services/privateChat/PrivateChatInfoService';
+import {
+    fetchChat, getPinMess, unPinMess,
+    blockUser, deleteAllMessage, toggleMute
+} from '../../services/privateChat/PrivateChatInfoService';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getUserIdFromToken } from '../../../../utils/auth';
 
 export default function PrivateChatInfoScreen() {
     const navigation = useNavigation();
     const route = useRoute();
-    const recipientName = route.params?.recipientName || 'null';
+    const chatId = route.params?.chatId || 'null';
+    const [newGroupName, setNewGroupName] = useState('');
+    const [avatar, setAvatar] = useState(null);
+    // const [userRole, setUserRole] = useState('');
+    const [pinMess, setPinMess] = useState([]);
     const [isMuted, setIsMuted] = useState(false);
     const [reportVisible, setReportVisible] = useState(false);
     const [blockVisible, setBlockVisible] = useState(false);
     const [reportReason, setReportReason] = useState('');
     const [pinVisible, setPinVisible] = useState(false);
 
+    const [userId, setUserId] = useState('');
+    const [receiverId, setReceiverId] = useState('');
+
+    const fetchChatInfo = async () => {
+        try {
+            const token = await AsyncStorage.getItem('token');
+            const chatInfo = await fetchChat(chatId, token); // Replace with actual token
+            setNewGroupName(chatInfo.name);
+            setAvatar(chatInfo.avatar);
+            const userId = getUserIdFromToken(token);
+            setUserId(userId);
+            setReceiverId(chatInfo.participants.find((member) => member.id !== userId).id);
+            try {
+                const pin_Mess = await getPinMess(chatId, token);
+                setPinMess(pin_Mess);
+            } catch (error) {
+                if (error.response?.status === 404) {
+                    // Xử lý lỗi 404 một cách yên lặng
+                    console.warn('No pinned message found.'); // Log cảnh báo nếu cần
+                    setPinMess('No pinned message'); // Đặt giá trị mặc định
+                } else {
+                    // Xử lý các lỗi khác
+                    console.warn('Error fetching pinned message:', error); // Log lỗi nếu cần
+                    setPinMess(null); // Đặt giá trị mặc định nếu xảy ra lỗi khác
+                }
+            }
+        } catch (error) {
+            console.error('Error fetching chat info:', error);
+        }
+    }
+
+    useEffect(() => {
+        fetchChatInfo();
+    }, []);
+
+    useFocusEffect(
+        useCallback(() => {
+            const unsubscribe = navigation.addListener('focus', () => {
+                fetchChatInfo();
+            });
+            return unsubscribe;
+        }, [navigation])
+    );
+
+    const handleToggleMute = async () => {
+        try {
+            const token = await AsyncStorage.getItem('token');
+            const response = await toggleMute(chatId, token);
+            setIsMuted(response.notify);
+            Alert.alert('Success', response.message);
+        } catch (error) {
+            console.error('Error toggling mute:', error);
+        }
+    };
+
+    const handleUnPinMessage = async (messageId) => {
+        try {
+            const token = await AsyncStorage.getItem('token');
+            const response = await unPinMess(messageId, token);
+            Alert.alert('Success', response.message);
+
+            const updatedPinMess = pinMess.filter((message) => message.id !== messageId);
+            setPinMess(updatedPinMess);
+        } catch (error) {
+            console.warn('Error unpinning message:', error);
+        }
+    };
+
+    const handleBlockUser = async () => {
+        try {
+            const token = await AsyncStorage.getItem('token');
+            const response = await blockUser(userId, receiverId, token);
+            setBlockVisible(false);
+            Alert.alert('Success', response.message);
+            navigation.navigate('HomeScreen');
+        } catch (error) {
+            console.warn('Error blocking user:', error);
+        }
+    }
+
     return (
         <View style={styles.container}>
             <SafeAreaProvider>
                 <View style={styles.chatInfo}>
-                    <Image source={require('../../../../assets/icon.png')} style={styles.avatar} />
-                    <Text style={styles.recipientName}>{recipientName}</Text>
+                    <Image source={{ uri: avatar }} style={styles.avatar} />
+                    <Text style={styles.recipientName}>{newGroupName}</Text>
                 </View>
 
                 {/* Actions */}
                 <View style={styles.actions}>
-                    <TouchableOpacity onPress={() => navigation.navigate('FindPrMessagesScreen')}>
+                    <TouchableOpacity onPress={() => navigation.navigate('FindPrMessagesScreen', { chatId })}>
                         <ActionButton icon="search" label="Find messages" />
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.actionButton} onPress={() => toggleMute(isMuted, setIsMuted)}>
+                    <TouchableOpacity style={styles.actionButton} onPress={() => handleToggleMute()}>
                         <Ionicons name={isMuted ? 'notifications' : 'notifications-off'} size={24} color="black" />
                         <Text style={styles.actionText}>{isMuted ? 'Unmute' : 'Mute'}</Text>
                     </TouchableOpacity>
@@ -58,9 +149,6 @@ export default function PrivateChatInfoScreen() {
                 </TouchableOpacity>
 
                 {/* Danger Zone */}
-                <TouchableOpacity onPress={() => setReportVisible(true)}>
-                    <OptionItem label="Report" icon="flag" textColor="red" />
-                </TouchableOpacity>
                 <TouchableOpacity onPress={() => setBlockVisible(true)}>
                     <OptionItem label="Block user" icon="ban" textColor="red" />
                 </TouchableOpacity>
@@ -107,7 +195,7 @@ export default function PrivateChatInfoScreen() {
                                 <Button
                                     title="Yes"
                                     color="red"
-                                    onPress={() => handleBlock(setBlockVisible, navigation)}
+                                    onPress={() => handleBlockUser()}
                                 />
                             </View>
                         </View>
@@ -119,8 +207,24 @@ export default function PrivateChatInfoScreen() {
                     <TouchableWithoutFeedback onPress={() => setPinVisible(false)}>
                         <View style={styles.modalContainer}>
                             <View style={styles.modalContent}>
-                                <Text style={styles.modalTitle}>Pin messenge</Text>
-                                <Text>...</Text>
+                                <Text style={styles.modalTitle}>Pinned Messages</Text>
+                                {Array.isArray(pinMess) && pinMess.length > 0 ? (
+                                    <ScrollView style={{ width: '100%' }}>
+                                        {pinMess.map((message, index) => (
+                                            <View key={index} style={styles.pinnedMessageContainer}>
+                                                <Text style={styles.pinnedMessageText}>{message.content}</Text>
+                                                <Button
+                                                    title="Un-pin"
+                                                    color="red"
+                                                    onPress={() => handleUnPinMessage(message.id)}
+                                                />
+                                            </View>
+                                        ))}
+                                    </ScrollView>
+                                ) : (
+                                    <Text style={styles.noPinnedMessageText}>No pinned messages</Text>
+                                )}
+                                <Button title="Close" onPress={() => setPinVisible(false)} />
                             </View>
                         </View>
                     </TouchableWithoutFeedback>
@@ -276,5 +380,25 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         marginTop: 20,
         width: '100%',
+    },
+    pinnedMessageContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#e0e0e0',
+        marginBottom: 5,
+    },
+    pinnedMessageText: {
+        flex: 1,
+        fontSize: 16,
+        color: '#555',
+    },
+    noPinnedMessageText: {
+        textAlign: 'center',
+        fontSize: 16,
+        color: '#888',
+        marginVertical: 10,
     },
 });
