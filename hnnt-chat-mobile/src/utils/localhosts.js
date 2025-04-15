0834dce2997e593d8acb7b340214ca1f653f7416
PhamLeThanhNhiet <nhiethiz@gmail.com>
2025-04-15 09:07:42 +0700

    feat: Update password validation logic in SignUpScreen and adjust password strength messages refactor:  cmt DateTimePicker import fix: Correct IP address for IP_WIFI in localhosts configuration chore: Add initial database migration script for Account, FriendRequest, and Chat tables

--- hnnt-chat-mobile/src/utils/localhosts.js
+++ hnnt-chat-mobile/src/utils/localhosts.js
@@ -1,5 +1,5 @@
 const IP_4G = '192.168.170.60';
-const IP_WIFI = '192.168.111.7';
+const IP_WIFI = '192.168.137.1';
 const defaultLocalhost = 'localhost';
 const DHCN_TV = '172.20.76.72';
 const BackAndWhite_floor_1 = '192.168.1.81';
