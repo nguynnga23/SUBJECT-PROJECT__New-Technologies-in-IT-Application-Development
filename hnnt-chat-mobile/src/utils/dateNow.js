bb3cf3fe85d5edce77b81091c9f069f4333fcfbd
PhamLeThanhNhiet <nhiethiz@gmail.com>
2025-04-16 18:50:51 +0700

    feat: Update GroupChatScreen styles and add utility function for current time retrieval

---
+++ hnnt-chat-mobile/src/utils/dateNow.js
@@ -0,0 +1,7 @@
+export const getCurrentTimeString = () => {
+    const now = new Date();
+    const hh = now.getHours().toString().padStart(2, '0');
+    const mm = now.getMinutes().toString().padStart(2, '0');
+    const ss = now.getSeconds().toString().padStart(2, '0');
+    return `${hh}-${mm}-${ss}`;
+}
