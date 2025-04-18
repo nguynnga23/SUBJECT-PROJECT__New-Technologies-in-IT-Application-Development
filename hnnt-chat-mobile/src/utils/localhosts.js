f3a6158e3d85f52ff22d9933703dbe5f3c17f26b
Nguyen Nga <nguyennga200x@gmail.com>
2025-04-18 17:50:22 +0700

    fix: Update IP_WIFI and localhost configuration in localhosts.js

--- hnnt-chat-mobile/src/utils/localhosts.js
+++ hnnt-chat-mobile/src/utils/localhosts.js
@@ -1,10 +1,10 @@
 const IP_4G = '192.168.170.60';
-const IP_WIFI = '192.168.101.11';
+const IP_WIFI = '192.168.40.101';
 const defaultLocalhost = 'localhost';
 const DHCN_TV = '172.20.77.153';
 const BackAndWhite_floor_1 = '192.168.1.81';
 const PORT = '4000';
 
-const localhost = `${DHCN_TV}:${PORT}`;
+const localhost = `${IP_WIFI}:${PORT}`;
 
 module.exports = { localhost };
