1422fc1515a8320a9b30935fa265ed00036776e4
Nguyen Nga <nguyennga200x@gmail.com>
2025-04-25 23:38:00 +0700

    fix: Add accountId to newParticipants in chatSlice

--- hnnt-chat-mobile/src/utils/localhosts.js
+++ hnnt-chat-mobile/src/utils/localhosts.js
@@ -1,5 +1,5 @@
 const IP_4G = '192.168.170.60';
-const IP_WIFI = '172.20.84.144';
+const IP_WIFI = '192.168.40.101';
 const defaultLocalhost = 'localhost';
 const DHCN_TV = '172.20.77.153';
 const BackAndWhite_floor_1 = '192.168.1.81';
