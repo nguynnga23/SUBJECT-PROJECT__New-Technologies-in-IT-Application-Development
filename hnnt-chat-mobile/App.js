627b3c7a9d0525a0d7ddc69378137e2bddf5b1d9
Nguyen Nga <nguyennga200x@gmail.com>
2025-05-10 16:31:29 +0700

    fix: Remove expo-notifications dependency and related push notification handling code

--- hnnt-chat-mobile/App.js
+++ hnnt-chat-mobile/App.js
@@ -4,15 +4,12 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { MenuProvider } from 'react-native-popup-menu';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
-import PushNotificationHandler from './src/common/components/PushNotificationHandler';
 
 export default function App() {
     return (
         <Provider store={store}>
             <MenuProvider>
                 <GestureHandlerRootView style={{ flex: 1 }}>
-                    {/* 👇 Đây là nơi "lắng nghe" notification */}
-                    <PushNotificationHandler />
                     <AppNavigator />
                 </GestureHandlerRootView>
             </MenuProvider>
